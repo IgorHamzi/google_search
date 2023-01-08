fc77d0c3e09b6793109431afbe2a793e4c4ac614
IgorHamzi <igorwhamzi@gmail.com>
2023-01-08 14:28:04 -0300

    feat: add function that implements the list

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+
+    print(instance)
+
+    for i in instance._data:
+        if i['nome_do_arquivo'] == path_file:
+            return ('arquivo já adicionado anteriomente')
+
+    lines = txt_importer(path_file)
+
+    file = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines
+    }
+
+    instance.enqueue(file)
 
 
 def remove(instance):
