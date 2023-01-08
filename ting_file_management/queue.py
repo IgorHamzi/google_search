13142d3f9ccc4c1e61e2f8bbaf91b2b350410370
IgorHamzi <igorwhamzi@gmail.com>
2023-01-08 12:54:16 -0300

    feat: implement a queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,33 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = deque()
+
+    def __repr__(self):
+        return f"Lista({list(self._data)})"
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        try:
+            return self._data.popleft()
+        except IndexError:
+            raise IndexError('lista vazia') from None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if not 0 <= index <= len(self._data) - 1:
+            raise IndexError
+
+        return self._data[index]
+
+    def is_empty(self):
+        return not bool(len(self._data))
+
+    def size(self):
+        return len(self._data)
