39acd2a873fddcd2be28e451cc6fadcfb5a64001
IgorHamzi <igorwhamzi@gmail.com>
2023-01-08 16:12:51 -0300

    feat: implements function to search words

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocorrencies = []
+    inventory_words = []
+    for i in instance._data:
+        for line in i['linhas_do_arquivo']:
+            if word.lower() in line.lower():
+                ocorrencies.append({'linha': i['linhas_do_arquivo'].index(line) + 1})
+        inventory_words.append({
+            "palavra": word,
+            "arquivo":  i['nome_do_arquivo'],
+            "ocorrencias": ocorrencies
+        })
+
+    if ocorrencies == []:
+        return ocorrencies
+    return inventory_words
 
 
 def search_by_word(word, instance):
