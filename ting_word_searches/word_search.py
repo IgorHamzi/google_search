dadf623ebc001b194f992340ecdc6b6ce857e187
IgorHamzi <igorwhamzi@gmail.com>
2023-01-08 16:19:22 -0300

    feat:  implements function that returns invetory

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocorrencies = []
+    inventory_words = []
+    for i in instance._data:
+        for line in i['linhas_do_arquivo']:
+            if word.lower() in line.lower():
+                ocorrencies.append({
+                    'linha': i['linhas_do_arquivo'].index(line) + 1,
+                    'conteudo': line
+                })
+        inventory_words.append({
+            "palavra": word,
+            "arquivo":  i['nome_do_arquivo'],
+            "ocorrencias": ocorrencies
+        })
+
+    if ocorrencies == []:
+        return ocorrencies
+    return inventory_words
